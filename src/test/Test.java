f88692e0aac4b0b994204ad7b43a061041720e66
classmatewang <baizhouwhp@163.com>
2021-11-24 12:47:12 +0800

    Initial commit

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @BelongsProject: Java_Source_Learn
+ * @BelongsPackage: test
+ * @Author: Wang Haipeng
+ * @CreateTime: 2021-11-24 10:39
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
